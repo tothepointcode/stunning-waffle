7d27c8e974ee1899c9bfdec0c2e854cefe33aa80
cobytab <terra.andoh@gmail.com>
2021-10-19 21:18:47 +0000

    feat: :construction: Colors updated.
    Yellow missing color added.

--- components/colors.tsx
+++ components/colors.tsx
@@ -2,7 +2,7 @@ export const colors = {
     primary: "#ef835d",
     secondary: "#2c365a",
     tertiary: "#85c6d8",
-    accent: "#85c6d8",
+    accent: "#fbcd77",
     white: "#fff",
     gray: "#D1D5DB"
 }
