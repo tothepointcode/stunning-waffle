01cc65fd312f39d4b9ac8362237c3a58480ca198
cobytab <terra.andoh@gmail.com>
2021-10-20 09:47:09 +0000

    feat: :construction: Welcome page created and cleared
    Duplicated GetStarted page.

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import React from "react";
 
 // screens
 import GetStarted from "./screens/GetStarted";
+import Welcome from "./screens/Welcome";
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <GetStarted />
+      <Welcome />
     </>
   );
 }
--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -9,48 +9,14 @@ import RegularButton from "../components/Buttons/RegularButton";
 import BigText from "../components/Texts/BigText";
 import SmallText from "../components/Texts/SmallText";
 
-const GetStartedContainer = styled(Container)`
-  background-color: ${colors.secondary};
-  justify-content: space-between;
+const WelcomeContainer = styled(Container)`
+  background-color: ${colors.gray};
   width: 100%;
   height: 100%;
-`;
-
-export const TopImage = styled.Image`
-  width: 100%;
-  height: 100%;
-  resize-mode: stretch;
-`;
-
-const TopSection = styled.View`
-  width: 100%;
-  flex: 1 1 auto;
-  max-height: 55%;
-`;
-const BottomSection = styled.View`
-  width: 100%;
-  padding: 25px;
-  flex: 1 1 auto;
-  justify-content: flex-end;
 `;
 
 const Welcome = () => {
-  return (
-    <GetStartedContainer>
-      <TopSection>
-        <TopImage source={require("./../assets/bgs/background_v1.png")} />
-      </TopSection>
-      <BottomSection>
-        <BigText textStyles={{ width: "70%" }}>
-          Best way to track your money
-        </BigText>
-        <SmallText textStyles={{ width: "70%" }}>
-          Best payment method, connects your money to your friends, family.
-        </SmallText>
-        <RegularButton>Get Started</RegularButton>
-      </BottomSection>
-    </GetStartedContainer>
-  );
+  return <WelcomeContainer></WelcomeContainer>;
 };
 
 export default Welcome;
