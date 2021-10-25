cca84326dfbabf6fd44160cac48f6c4c86608087
cobytab <terra.andoh@gmail.com>
2021-10-25 14:14:02 +0000

    feat: :construction: Font sizing adjustments made to GetStarted Screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 
+// custom font
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
+
 // React Navigation
 import RootStack from "./navigators/RootStack";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "Lato-Black": require("./assets/fonts/Lato-Black.ttf"),
+    "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
+    "Lato-Light": require("./assets/fonts/Lato-Light.ttf"),
+    "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <>
       <StatusBar style="auto" />
